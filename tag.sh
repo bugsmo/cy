d36524e2df06d684d9bd0d55b56054960ade0a09
bugsmo <kalandramo@gmail.com>
2024-04-28 10:54:44 +0800

    modify: trans

--- tag.sh
+++ tag.sh
@@ -1,3 +1,3 @@
-git tag v1.1.6
+git tag v1.1.7
 
 git push origin --tags
--- utils/trans/trans.go
+++ utils/trans/trans.go
@@ -1,5 +1,19 @@
 package trans
 
+import (
+	"time"
+
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
 func Int32ToPointer(a int32) *int32 {
 	return &a
+}
+
+// TimeToTimestamppb time.Time -> timestamppb.Timestamp
+func TimeToTimestamppb(tm *time.Time) *timestamppb.Timestamp {
+	if tm != nil {
+		return timestamppb.New(*tm)
+	}
+	return nil
 }
